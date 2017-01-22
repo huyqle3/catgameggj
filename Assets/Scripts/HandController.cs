f258872ac6cacf5cf520cae551f24f5e2f7505a2
Caleb Markley <cablay91@gmail.com>
2017-01-22 02:20:35 -0800

    Add to score for caught fish.

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class HandController : MonoBehaviour {
 
     ParticleSystem ps;
+    FishScore fishScore;
 
     void Awake()
     {
         ps = GetComponent<ParticleSystem>();
+        fishScore = GameObject.Find("FishScore").GetComponent<FishScore>();
     }
 
     void OnTriggerEnter(Collider other)
 	{
         ps.Emit(15);
-		Destroy (other.gameObject);
+        fishScore.score += 1;
+        Destroy (other.gameObject);
 	}
 }
